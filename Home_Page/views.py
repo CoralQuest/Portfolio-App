875f2e51a108d1e26f91a82cd471546ee968acec
dmaracle <56905948+dmaracle@users.noreply.github.com>
2024-09-12 15:55:59 -0700

    Added Navbar, anchors to sections, and clickable image to projects

--- Home_Page/views.py
+++ Home_Page/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def project_detail(request):
+    return render(request, 'project_detail.html')
